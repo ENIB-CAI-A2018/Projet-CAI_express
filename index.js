d897c2cb1cc0e831a3797d6ca128cd6ef76b27f0
yoann2968 <yoann.spriet@gmail.com>
2018-12-04 16:16:14 +0100

    Récupération des données des profs avec la recherche /prof/nom

--- index.js
+++ index.js
@@ -18,25 +18,58 @@ var findCours = function(db, courList,  callback) {
     });
  };
 
+ var findprofesseur = function(db, profId,  callback) {
+   const cursor =db.collection(profId).find({});
+   var prof;
+   cursor.each(function(err, doc) {
+      assert.equal(err, null);
+      if (doc != null) {
+         prof = doc;
+      } else {
+         callback(prof);
+      }
+   });
+};
+
  app.get('/cours', function (req, res) {
     console.log('Received request for cours from', req.ip)
-  
     MongoClient.connect(url, function(err, dataBase) {
       assert.equal(null, err);
       var courList = [];
       const db=dataBase.db('Project-CAI');
-
-      console.log("Demande des cours");
       findCours(db, courList, function() {
         res.json(courList);
         dataBase.close();
       });
     });
   });
+  
+app.get('/prof/:profId', function (req, res) {
+   console.log('Received request for '+req.param('profId')+' from', req.ip)
+   MongoClient.connect(url, function(err, dataBase) {
+     assert.equal(null, err);
+     const db=dataBase.db('Project-CAI');
+     findprofesseur(db, req.param('profId'),  function(prof) {
+       console.log(prof)
+       res.json(prof);
+       dataBase.close();
+     });
+   });
+ });
+
+ app.get('/test/:testId', function (req, res) {
+   console.log('Received request for '+req.param('testId')+' from', req.ip)
+   MongoClient.connect(url, function(err, dataBase) {
+     assert.equal(null, err);
+     const db=dataBase.db('Project-CAI');
+     createProf(db, req.param('profId'), function() {
+       dataBase.close();
+     });
+   });
+ });
 
  var url = 'mongodb://localhost:27017/Project-CAI';
 
- 
 // Use connect method to connect to the Server
 MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
