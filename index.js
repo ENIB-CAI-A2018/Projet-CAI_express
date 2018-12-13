a3fd9ee0513d09609ddb0603faa30b7674a4427c
yoann2968 <yoann.spriet@gmail.com>
2018-12-13 16:43:10 +0100

    ajout de réception d'un json avec post

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express = require('express');
-var cors = require('cors')
+var cors = require('cors');
+var bodyParser = require("body-parser");
 var app = express();
 
-app.use(cors())
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 var MongoClient = require('mongodb').MongoClient, assert = require('assert');
 
